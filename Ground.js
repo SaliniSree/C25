35948cf4ef2b9d9b71e859399fe425b6a224c31d
SaliniSree <71487918+SaliniSree@users.noreply.github.com>
2020-10-15 23:21:14 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,8 @@
+class Ground extends BaseClass{
+    constructor(x,y,width,height) {
+      super(x,y,width,height)
+      Matter.Body.setStatic(this.body, true)
+      this.image = loadImage('sprites/base.png')
+    }
+    
+  };
